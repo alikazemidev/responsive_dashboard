1f40415e345ce7149e5c09538a3ad9985a4e1e7f
alikz <ali6316k@gmail.com>
2023-06-11 20:11:10 +0330

    add some code

--- lib/responsive/table_scaffold.dart
+++ lib/responsive/table_scaffold.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash/constants/constants.dart';
 
+import '../utils/my_box.dart';
+import '../utils/my_tile.dart';
+
 class TabletScaffold extends StatefulWidget {
   const TabletScaffold({super.key});
 
@@ -11,10 +14,43 @@ class TabletScaffold extends StatefulWidget {
 class _TabletScaffoldState extends State<TabletScaffold> {
   @override
   Widget build(BuildContext context) {
-     return  Scaffold(
+    return Scaffold(
       backgroundColor: myDefaultBg,
-      appBar:myAppBar,
+      appBar: myAppBar,
       drawer: myDrawer,
+      body: Column(
+        children: [
+          // 4box
+          Expanded(
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: SizedBox(
+                width: double.infinity,
+                child: GridView.builder(
+                  itemCount: 4,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                  ),
+                  itemBuilder: (context, index) {
+                    return const MYBox();
+                  },
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index) {
+                return MyTile();
+              },
+            ),
+          )
+          // tile
+        ],
+      ),
     );
   }
 }
